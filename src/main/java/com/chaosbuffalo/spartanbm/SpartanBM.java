9bc08b076c0036affeb465509c5762c7a4ef73c2
Trinsdar <westbound1715@gmail.com>
2018-10-14 23:26:48 -0400

    changed version to 0.2 and made it easier to change the version in the future

--- src/main/java/com/chaosbuffalo/spartanbm/SpartanBM.java
+++ src/main/java/com/chaosbuffalo/spartanbm/SpartanBM.java
@@ -13,7 +13,7 @@ public class SpartanBM
 {
     public static final String MODID = "spartanbm";
     public static final String NAME = "SpartanWeaponry-BaseMetals";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "@VERSION@";
 
     private static Logger logger;
 
